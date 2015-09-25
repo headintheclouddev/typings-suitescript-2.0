c7e69b68ef26e5b8fc8dcb8e762034f2606f6456
Robin Mitchell <robin.mitchell@headintheclouddev.com>
2015-09-25 13:44:08 -0700

    Added interfaces for Parser.toString and Parser.fromString

--- N/xml.d.ts
+++ N/xml.d.ts
@@ -1,5 +1,18 @@
 /// <reference path="../typings/tsd.d.ts" />
 
+interface ParserFromStringOptions {
+  text: string;
+}
+
+interface ParserToStringOptions {
+  document: XMLDocument;
+}
+
+interface Parser {
+  fromString: (options: ParserFromStringOptions) => XMLDocument;
+  toString: (opations: ParserToStringOptions) => string;
+}
+
 declare module N {
     var xml:any;
 }
