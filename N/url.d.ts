26bdaa599ad67b1c4cbb0bbfaa4a1765dc9abb28
Robin Mitchell <robin.mitchell@headintheclouddev.com>
2015-09-27 21:39:23 -0700

    Added the URL module

---
+++ N/url.d.ts
@@ -0,0 +1,40 @@
+/// <reference path="../typings/tsd.d.ts" />
+
+interface formatOptions {
+  domain: string;
+  parameters: Object;
+}
+
+interface resolveRecordOptions {
+  recordType: string;
+  recordId: string;
+  isEditMode?: boolean;
+  params?: Object;
+}
+
+interface resolveScriptOptions {
+  scriptId: string;
+  deploymentId: string;
+  returnExternalUrl?: boolean;
+  params?: Object;
+}
+
+interface resolveTaskLinkOptions {
+  id: string;
+  parameters?: Object;
+}
+
+interface URLModule {
+  format: (options: formatOptions) => string;
+  resolveRecord: (options: resolveRecordOptions) => string;
+  resolveScript: (options: resolveScriptOptions) => string;
+  resolveTaskLink: (options: resolveTaskLinkOptions) => string;
+}
+
+declare module N {
+    var url: URLModule;
+}
+
+declare module 'N/url' {
+    export = N.url;
+}
