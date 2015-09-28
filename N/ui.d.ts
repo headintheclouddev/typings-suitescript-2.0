fb428d845cde1e7e16f652b7ed5a4d53d3a1b02a
Robin Mitchell <robin.mitchell@headintheclouddev.com>
2015-09-27 21:39:07 -0700

    UI Module: Added some Form and Button stuff

--- N/ui.d.ts
+++ N/ui.d.ts
@@ -1,7 +1,33 @@
 /// <reference path="../typings/tsd.d.ts" />
 
+interface addButtonOptions {
+  id?: string;
+  label: string;
+  script?: string;
+}
+
+interface uiButton {
+  isDisabled: boolean;
+  isHidden: boolean;
+  label: string;
+}
+
+interface uiForm {
+  addButton: (options: addButtonOptions) => uiButton;
+}
+
+interface uiModule {
+  AssistantStep: Object;
+  Button: Object;
+  Field: Object;
+  FieldGroup: Object;
+  Form: uiForm;
+  Sublist: Object;
+  Tab: Object;
+}
+
 declare module N {
-    var ui:any;
+    var ui: uiModule;
 }
 
 declare module 'N/ui' {
