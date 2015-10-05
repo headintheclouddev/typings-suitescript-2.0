f70b7d76e499f6214c9e27d0601a6649f374c54f
Robin Mitchell <robin.mitchell@headintheclouddev.com>
2015-10-04 20:39:57 -0700

    Script Entries: 1. Made each script trigger function its own interface 2. Finished writing all the Client script ones

--- scriptTypes/scriptEntry.d.ts
+++ scriptTypes/scriptEntry.d.ts
@@ -1,16 +1,65 @@
 /// <reference path="../N/record.d.ts" />
 
-interface NS2_Client_PageInitContext {
-	currentRecord: Record;
-	mode: string;
-}
-
 interface NS2_Client_FieldChangeContext {
 	currentRecord: Record;
 	sublistId: string;
 	fieldId: string;
 	lineNum: string;
 	columnNum: string;
+}
+
+interface NS2_Client_FieldChanged {
+  (scriptContext?: NS2_Client_FieldChangeContext): void;
+}
+
+interface NS2_Client_LineInitContext {
+	currentRecord: Record;
+	sublistId: string;
+}
+
+interface NS2_Client_LineInit {
+  (scriptContext?: NS2_Client_LineInitContext): void;
+}
+
+interface NS2_Client_PageInitContext {
+	currentRecord: Record;
+	mode: string;
+}
+
+interface NS2_Client_PageInit {
+  (scriptContext?: NS2_Client_PageInitContext): void;
+}
+
+interface NS2_Client_PostSourceContext {
+  currentRecord: Record;
+  sublistId: string;
+  fieldId: string;
+}
+
+interface NS2_Client_PostSource {
+  (scriptContext?: NS2_Client_PostSourceContext): void;
+}
+
+interface NS2_Client_SaveRecord {
+  (): boolean;
+}
+
+interface NS2_Client_SublistChangedContext {
+  currentRecord: Record;
+  sublistId: string;
+}
+
+interface NS2_Client_SublistChanged {
+  (scriptContext?: NS2_Client_SublistChangedContext): void;
+}
+
+interface NS2_Client_ValidateDeleteContext {
+  currentRecord: Record;
+  sublistId: string;
+}
+
+interface NS2_Client_ValidateDelete {
+  (scriptContext?: NS2_Client_ValidateDeleteContext): boolean;
 }
 
 interface NS2_Client_ValidateFieldContext {
@@ -21,8 +70,24 @@ interface NS2_Client_ValidateFieldContext {
   columnNum?: string;
 }
 
-interface ScriptEntry {
-  NS2_Client_PageInit: (scriptContext: NS2_Client_PageInitContext) => void;
-  NS2_Client_FieldChanged: (scriptContext: NS2_Client_FieldChangeContext) => void;
-  NS2_Client_ValidateField: (scriptContext: NS2_Client_ValidateFieldContext) => boolean;
+interface NS2_Client_ValidateField {
+  (scriptContext?: NS2_Client_ValidateFieldContext): boolean;
+}
+
+interface NS2_Client_ValidateInsertContext {
+  currentRecord: Record;
+  sublistId: string;
+}
+
+interface NS2_Client_ValidateInsert {
+  (scriptContext?: NS2_Client_ValidateInsertContext): boolean;
+}
+
+interface NS2_Client_ValidateLineContext {
+  currentRecord: Record;
+  sublistId: string;
+}
+
+interface NS2_Client_ValidateLine {
+  (scriptContext?: NS2_Client_ValidateLineContext): boolean;
 }
