8f3e139d7cf6f470618f16a7216d8b4737a349f4
Robin Mitchell <robin.mitchell@headintheclouddev.com>
2015-10-21 17:24:54 +0100

    HTTPS: Filled in more stuff

--- N/https.d.ts
+++ N/https.d.ts
@@ -1,7 +1,53 @@
 /// <reference path="../typings/tsd.d.ts" />
 
+interface CreateSecureKeyOptions {
+  encoding: string;
+  guid: string;
+}
+
+interface CreateSecureStringOptions {
+  input: string;
+  inputEncoding: string;
+}
+
+interface AppendStringOptions {
+  input: string;
+  encoding: string;
+}
+
+interface AppendSecureStringOptions {
+  token: SecureString;
+}
+
+interface ConvertEncodingOptions {
+  toEncoding: string;
+}
+
+interface HashOptions {
+  algorithm: string;
+}
+
+interface HmacOptions {
+  algorithm: string;
+  key: SecretKey;
+}
+
+interface SecureString {
+  appendString: (options: AppendStringOptions) => SecureString;
+  appendSecureString: (options: AppendSecureStringOptions) => SecureString;
+  convertEncoding: (options: ConvertEncodingOptions) => SecureString;
+  hash: (options: HashOptions) => SecureString;
+  hmac: (options: HmacOptions) => SecureString;
+  toString: () => string;
+}
+
+interface HttpsModule {
+  createSecureKey: (options: CreateSecureKeyOptions) => SecretKey;
+  createSecureString: (options: CreateSecureStringOptions) => any;
+}
+
 declare module N {
-    var https:any;
+    var https: HttpsModule;
 }
 
 declare module 'N/https' {
