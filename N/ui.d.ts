bd7ce855f8ca03ad8549eba0b16a1d0a223f259c
Robin Mitchell <robin.mitchell@headintheclouddev.com>
2015-10-24 10:17:04 +0100

    UI Module: Fix FieldDisplayTypes and FieldTypes

--- N/ui.d.ts
+++ N/ui.d.ts
@@ -9,7 +9,7 @@ interface AddButtonOptions {
 interface AddFieldOptions {
   id: string;
   label: string;
-  type: FieldType;
+  type: FieldTypeList;
   source?: string;
   tab?: string;
 }
@@ -46,40 +46,44 @@ interface UIField {
   displayType: FieldDisplayType;
 }
 
+interface FieldDisplayValue {}
+
 interface FieldDisplayType {
-  DISABLED: string;
-  ENTRY: string;
-  HIDDEN: string;
-  INLINE: string;
-  NORMAL: string;
-  READONLY: string;
+  DISABLED: FieldDisplayValue;
+  ENTRY: FieldDisplayValue;
+  HIDDEN: FieldDisplayValue;
+  INLINE: FieldDisplayValue;
+  NORMAL: FieldDisplayValue;
+  READONLY: FieldDisplayValue;
 }
 
-interface FieldType {
-  CHECKBOX: string;
-  CURRENCY: string;
-  DATE: string;
-  DATETIMETZ: string;
-  EMAIL: string;
-  FILE: string;
-  FLOAT: string;
-  HELP: string;
-  INLINEHTML: string;
-  INTEGER: string;
-  IMAGE: string;
-  LABEL: string;
-  LONGTEXT: string;
-  MULTISELECT: string;
-  PASSPORT: string;
-  PERCENT: string;
-  PHONE: string;
-  SELECT: string;
-  RADIO: string;
-  RICHTEXT: string;
-  TEXT: string;
-  TEXTAREA: string;
-  TIMEOFDAY: string;
-  URL: string;
+interface FieldTypeValue {}
+
+interface FieldTypeList {
+  CHECKBOX: FieldTypeValue;
+  CURRENCY: FieldTypeValue;
+  DATE: FieldTypeValue;
+  DATETIMETZ: FieldTypeValue;
+  EMAIL: FieldTypeValue;
+  FILE: FieldTypeValue;
+  FLOAT: FieldTypeValue;
+  HELP: FieldTypeValue;
+  INLINEHTML: FieldTypeValue;
+  INTEGER: FieldTypeValue;
+  IMAGE: FieldTypeValue;
+  LABEL: FieldTypeValue;
+  LONGTEXT: FieldTypeValue;
+  MULTISELECT: FieldTypeValue;
+  PASSPORT: FieldTypeValue;
+  PERCENT: FieldTypeValue;
+  PHONE: FieldTypeValue;
+  SELECT: FieldTypeValue;
+  RADIO: FieldTypeValue;
+  RICHTEXT: FieldTypeValue;
+  TEXT: FieldTypeValue;
+  TEXTAREA: FieldTypeValue;
+  TIMEOFDAY: FieldTypeValue;
+  URL: FieldTypeValue;
 }
 
 interface uiForm {
@@ -95,6 +99,8 @@ interface uiModule {
   Form: uiForm;
   Sublist: Object;
   Tab: Object;
+  FieldDisplayType: FieldDisplayType;
+  FieldType: FieldTypeList;
 }
 
 declare module N {
