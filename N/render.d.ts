4a23d8c5801c8e064b2704666b8d3c7ab626d65f
Robin Mitchell <robin.mitchell@headintheclouddev.com>
2015-10-26 13:20:42 +0000

    Render module: Added Statement pdf

--- N/render.d.ts
+++ N/render.d.ts
@@ -1,7 +1,20 @@
 /// <reference path="../typings/tsd.d.ts" />
 
+interface StatementOptions {
+  entityId: number;
+  printMode?: string;
+  formId?: number;
+  startDate?: Date;
+  statementDate?: Date;
+  TransactionsOnly?: boolean;
+}
+
+interface RenderModule {
+  statement(options: StatementOptions): NSFile;
+}
+
 declare module N {
-    var render:any;
+    var render: RenderModule;
 }
 
 declare module 'N/render' {
