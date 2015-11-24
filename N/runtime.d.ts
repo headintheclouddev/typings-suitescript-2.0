5ebf34d08ba52ef02521a7f38bd3e4b6ed5fc67e
Robin Mitchell <robin.mitchell@headintheclouddev.com>
2015-11-23 16:14:47 -0800

    Runtime: Finished it.

--- N/runtime.d.ts
+++ N/runtime.d.ts
@@ -1,81 +1,93 @@
 /// <reference path="../typings/tsd.d.ts" />
 
+interface GetParameterOptions {
+    name: string;
+}
+
+interface SetOptions {
+    name: string;
+    value: string;
+}
+
 interface Script {
-  deploymentId: string;
-  id: string;
-  logLevel: string;
-  percentComplete: number;
-  bundleIds: string[];
-  // TODO: Define the Script methods
+    getParameter(options: GetParameterOptions): number|Date|string|string[];
+    getRemainingUsage(): number;
+    deploymentId: string;
+    id: string;
+    logLevel: string;
+    percentComplete: number;
+    bundleIds: string[];
 }
 
 interface Session {
-  
+    get(options: GetParameterOptions): string;
+    set(options: SetOptions): void;
 }
 
 interface User {
-  // TODO: Define getPermissions and getPreference
-  department: number;
-  email: string;
-  id: number;
-  location: number;
-  name: string;
-  role: number;
-  roleCenter: number;
-  roleId: string; // text ID
-  subsidiary: number;
+    getPermission(options: GetParameterOptions): string;
+    getPreference(options: GetParameterOptions): string;
+    department: number;
+    email: string;
+    id: number;
+    location: number;
+    name: string;
+    role: number;
+    roleCenter: number;
+    roleId: string; // text ID
+    subsidiary: number;
 }
 
 interface FeatureOptions {
-  name: string;
+    name: string;
 }
 
 interface ContextTypes {
-  USER_INTERFACE: string;
-  WEBSERVICES: string;
-  WEBSTORE: string;
-  PORTLET: string;
-  SCHEDULED: string;
-  SUITELET: string;
-  CSV_IMPORT: string;
-  CUSTOM_MASSUPDATE: string;
-  WORKFLOW: string;
-  USEREVENT: string;
+    USER_INTERFACE: string;
+    WEBSERVICES: string;
+    WEBSTORE: string;
+    PORTLET: string;
+    SCHEDULED: string;
+    SUITELET: string;
+    CSV_IMPORT: string;
+    CUSTOM_MASSUPDATE: string;
+    WORKFLOW: string;
+    USEREVENT: string;
 }
 
 interface EnvTypes {
-  SANDBOX: string;
-  PRODUCTION: string;
-  BETA: string;
-  INTERNAL: string;
+    SANDBOX: string;
+    PRODUCTION: string;
+    BETA: string;
+    INTERNAL: string;
 }
 
 interface Permissions {
-  FULL: string;
-  EDIT: string;
-  CREATE: string;
-  VIEW: string;
-  NONE: string;
+    FULL: string;
+    EDIT: string;
+    CREATE: string;
+    VIEW: string;
+    NONE: string;
 }
 
 interface RuntimeModule {
-  accountId: string;
-  envType: string;
-  executionContext: string;
-  queueCount: number;
-  version: string;
-  getCurrentScript: () => Script;
-  getCurrentSession: () => Session;
-  getCurrentUser: () => User;
-  isFeatureInEffect: (options: FeatureOptions) => boolean;
-  ContextType: ContextTypes;
-  EnvType: EnvTypes;
+    accountId: string;
+    envType: string;
+    executionContext: string;
+    queueCount: number;
+    version: string;
+    getCurrentScript: () => Script;
+    getCurrentSession: () => Session;
+    getCurrentUser: () => User;
+    isFeatureInEffect: (options: FeatureOptions) => boolean;
+    ContextType: ContextTypes;
+    EnvType: EnvTypes;
 }
 
 declare module N {
-  var runtime: RuntimeModule;
+    var runtime: RuntimeModule;
 }
 
 declare module 'N/runtime' {
-  export = N.runtime;
+    export = N.runtime;
 }
