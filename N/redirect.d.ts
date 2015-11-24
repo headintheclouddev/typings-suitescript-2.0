9c49017175ff04fc5ccb94989757fbf0667d41d7
Robin Mitchell <robin.mitchell@headintheclouddev.com>
2015-11-23 16:14:14 -0800

    Redirect module: Wrote it.

--- N/redirect.d.ts
+++ N/redirect.d.ts
@@ -1,11 +1,51 @@
 /// <reference path="../typings/tsd.d.ts" />
+/// <reference path="../N/search.d.ts" />
+
+interface RedirectOptions {
+    url: string;
+    parameters?: Object;
+}
+
+interface ToRecordOptions {
+    id: string;
+    type: string;
+    isEditMode?: boolean;
+    parameters?: Object;
+}
+
+interface ToSavedSearchOptions {
+    id: number;
+}
+
+interface ToSearchOptions {
+    search: Search;
+}
+
+interface ToSuiteletOptions {
+    scriptId: string;
+    deploymentId: string;
+    isExternal?: boolean;
+    parameters?: Object;
+}
+
+interface ToTaskLinkOptions {
+    id: string;
+    parameters?: Object;
+}
 
 interface RedirectModule {
-  
+    redirect(options: RedirectOptions): void;
+    toRecord(options: ToRecordOptions): void;
+    toSavedSearch(options: ToSavedSearchOptions): void;
+    toSavedSearchResult(options: ToSavedSearchOptions): void;
+    toSearch(options: ToSearchOptions): void;
+    toSearchResult(options: ToSearchOptions): void;
+    toSuitelet(options: ToSuiteletOptions): void;
+    toTaskLink(options: ToTaskLinkOptions): void;
 }
 
 declare module N {
-    var redirect: any;
+    var redirect: RedirectModule;
 }
 
 declare module 'N/render' {
