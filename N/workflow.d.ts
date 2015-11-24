9a7d82c33389d768be1e90462e598f80efcbf41a
Robin Mitchell <robin.mitchell@headintheclouddev.com>
2015-11-23 16:15:55 -0800

    Workflow module: Wrote it

--- N/workflow.d.ts
+++ N/workflow.d.ts
@@ -1,7 +1,27 @@
 /// <reference path="../typings/tsd.d.ts" />
 
+interface InitiateOptions {
+    recordType: number;
+    recordId: string|number;
+    workflowType: string|number;
+    defaultValues?: Object;
+}
+
+interface TriggerOptions {
+    recordType: number;
+    recordId: string|number;
+    workflowType: string|number;
+    defaultValues?: Object;
+    actionId?: string|number;
+}
+
+interface WorkflowModule {
+    initiate(options: InitiateOptions): number;
+    trigger(options: TriggerOptions): number;
+}
+
 declare module N {
-    var workflow:any;
+    var workflow: WorkflowModule;
 }
 
 declare module 'N/workflow' {
