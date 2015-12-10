9c71e6eb1ff24cc35e72cfec987d0e43e036aef4
Robin Mitchell <robin.mitchell@headintheclouddev.com>
2015-12-09 16:56:06 -0800

    HTTPS: fixed spacing, reference crypto

--- N/https.d.ts
+++ N/https.d.ts
@@ -1,56 +1,57 @@
 /// <reference path="../typings/tsd.d.ts" />
+/// <reference path="crypto.d.ts" />
 /// <reference path="http.d.ts" />
 
 interface CreateSecureKeyOptions {
-  encoding: string;
-  guid: string;
+    encoding: string;
+    guid: string;
 }
 
 interface CreateSecureStringOptions {
-  input: string;
-  inputEncoding: string;
+    input: string;
+    inputEncoding: string;
 }
 
 interface AppendStringOptions {
-  input: string;
-  encoding: string;
+    input: string;
+    encoding: string;
 }
 
 interface AppendSecureStringOptions {
-  token: SecureString;
+    token: SecureString;
 }
 
 interface ConvertEncodingOptions {
-  toEncoding: string;
+    toEncoding: string;
 }
 
 interface HashOptions {
-  algorithm: string;
+    algorithm: string;
 }
 
 interface HmacOptions {
-  algorithm: string;
-  key: SecretKey;
+    algorithm: string;
+    key: SecretKey;
 }
 
 interface SecureString {
-  appendString: (options: AppendStringOptions) => SecureString;
-  appendSecureString: (options: AppendSecureStringOptions) => SecureString;
-  convertEncoding: (options: ConvertEncodingOptions) => SecureString;
-  hash: (options: HashOptions) => SecureString;
-  hmac: (options: HmacOptions) => SecureString;
-  toString: () => string;
+    appendString: (options: AppendStringOptions) => SecureString;
+    appendSecureString: (options: AppendSecureStringOptions) => SecureString;
+    convertEncoding: (options: ConvertEncodingOptions) => SecureString;
+    hash: (options: HashOptions) => SecureString;
+    hmac: (options: HmacOptions) => SecureString;
+    toString: () => string;
 }
 
 interface HttpsModule {
-  createSecureKey: (options: CreateSecureKeyOptions) => SecretKey;
-  createSecureString: (options: CreateSecureStringOptions) => any;
-  // This module encapsulates the HTTP module (any better way to do this?):
-  get: (options: GetOptions) => ClientResponse;
-  delete: (options: DeleteOptions) => ClientResponse;
-  request: (options: RequestOptions) => ClientResponse;
-  post: (options: PostOptions) => ClientResponse;
-  put: (options: PutOptions) => ClientResponse;
+    createSecureKey: (options: CreateSecureKeyOptions) => SecretKey;
+    createSecureString: (options: CreateSecureStringOptions) => any;
+    // This module encapsulates the HTTP module (any better way to do this?):
+    get: (options: GetOptions) => ClientResponse;
+    delete: (options: DeleteOptions) => ClientResponse;
+    request: (options: RequestOptions) => ClientResponse;
+    post: (options: PostOptions) => ClientResponse;
+    put: (options: PutOptions) => ClientResponse;
 }
 
 declare module N {
