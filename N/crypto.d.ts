7874010513d2af1e68adec917f1608d49f8e463e
Nicolas Wolf <nicolas.wolf@headintheclouddev.com>
2016-05-09 16:25:49 -0700

    N/crypto - Added documentation for enums and methods.

--- N/crypto.d.ts
+++ N/crypto.d.ts
@@ -1,65 +1,140 @@
 /// <reference path="../typings/tsd.d.ts" />
 /// <reference path="./encode.d.ts" />
 
+/**
+ * Encapsulates a cipher.
+ */
 interface Cipher {
-  final: (options: FinalOptions) => CipherPayload;
-  update: (options: UpdateOptions) => void;
+  /**
+   * Description TBA (5/9/2016)
+   */
+  final (options: FinalOptions): CipherPayload;
+  /**
+   * Description TBA (5/9/2016)
+   */
+  update (options: UpdateOptions): void;
 }
-
+/**
+ * Encapsulates a cipher payload.
+ */
 interface CipherPayload {
   encoding: string;
-  iv: string;
+  iv: number;
 }
 
 interface Decipher {
-  final: (options: FinalOptions) => string;
-  update: (options: UpdateOptions) => void;
+  /**
+   * Description TBA (5/9/2016)
+   */
+  final (options: FinalOptions): string;
+  /**
+   * Method used to update decipher data with the specified encoding.
+   */
+  update (options: UpdateOptions): void;
 }
 
 interface Hash {
-  digest: (options: FinalOptions) => string;
-  update: (options: UpdateOptions) => void;
+  /**
+   * Calculates the digest of the data to be hashed.
+   */
+  digest (options: FinalOptions): string;
+  /**
+   * Method used to update hash data with the encoding specified.
+   */
+  update (options: UpdateOptions): void;
 }
 
 interface Hmac {
-  digest: (options: FinalOptions) => string;
-  update: (options: UpdateOptions) => void;
+  /**
+   * Gets the computed digest.
+   */
+  digest (options: FinalOptions): string;
+  /**
+   * Method used to update the hmac data with the encoding specified.
+   */
+  update (options: UpdateOptions): void;
 }
 
 interface SecretKey {
-  // apparently its still a secret.
-}
-
-interface CreateCipherOptions {
-  algorithm: HashAlgTypes;
-  key: SecretKey;
-  blockCipherMode: string;
-  padding: PaddingTypes;
+  guid: string;
+  encoding: string;
 }
 
 interface FinalOptions {
+  /**
+   * The output encoding for a crypto.CipherPayload object.
+   */
   outputEncoding: string;
 }
 
 interface UpdateOptions {
+  /** 
+   * The cipher data to be updated.
+   */
   input: string;
-  inputEncoding: Encoding;
+  /**
+   * The input encoding using encode.Encoding enum. Default: UTF_8.
+   */
+  inputEncoding?: Encoding;
+}
+
+interface CreateCipherOptions {
+  /**
+   * The hash algorithm. Set the value using thecrypto.Hash enum.
+   */
+  algorithm: HashAlgTypes;
+  /**
+   * The crypto.SecretKey object.
+   */
+  key: SecretKey;
+  /**
+   * The encryption mode for the cipher block.
+   */
+  blockCipherMode: string;
+  /**
+   * The padding for the cipher. Set the value using the crypto.Padding enum.
+   */
+  padding: PaddingTypes;
 }
 
 interface CreateDecipherOptions {
+  /**
+   * The hash algorithm. Set by the crypto.Hash enum.
+   */
   algorithm: HashAlgTypes;
+  /**
+   * The crypto.SecretKey object.
+   */
   key: SecretKey;
+  /**
+   * Description TBA (5/9/2016)
+   */
   blockCipherMode: string;
+  /**
+   * Description TBA (5/9/2016)
+   */
   padding: PaddingTypes;
+  /**
+   * Description TBA (5/9/2016)
+   */
   iv: string;
 }
 
 interface CreateHashOptions {
+  /**
+   * The hash algorithm. Set by the crypto.Hash enum.
+   */
   algorithm: HashAlgTypes;
 }
 
 interface CreateHmacOptions {
+  /**
+   * The hash algorithm. Set by the crypto.Hash enum.
+   */
   algorithm: HashAlgTypes;
+  /**
+   * The crypto.SecretKey object.
+   */
   key: SecretKey;
 }
 
@@ -91,13 +166,30 @@ interface CryptoModule {
   Hash: Hash;
   Hmac: Hmac;
   SecretKey: SecretKey;
-  createCipher: (options: CreateCipherOptions) => Cipher;
-  createDecipher: (options: CreateDecipherOptions) => Decipher;
-  createHash: (options: CreateHashOptions) => Hash;
-  createSecretKey: (options: CreateSecretKeyOptions) => SecretKey;
   EncryptionAlg: EncryptionAlgTypes;
   HashAlg: HashAlgTypes;
   Padding: PaddingTypes;
+  /**
+   * Method used to create and return a new crypto.EncryptionAlg object.
+   * NOTE: we are NOT returning an EncryptionAlg, as it is not a real object. (5/9/2016)
+   */
+  createCipher (options: CreateCipherOptions): Cipher;
+  /**
+   * Method used to create a new crypto.Decipher object.
+   */
+  createDecipher (options: CreateDecipherOptions): Decipher;
+  /**
+   * Method used to create a new crypto.Hash object.
+   */
+  createHash (options: CreateHashOptions): Hash;
+  /**
+   * Method used to create a new crypto.Hmac object.
+   */
+  createHmac (options: CreateHmacOptions): Hmac;
+  /**
+   * Method used to create a new crypto.SecretKey object.
+   */
+  createSecretKey (options: CreateSecretKeyOptions): SecretKey;
 }
 
 declare module N {
