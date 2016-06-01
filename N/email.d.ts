f2d973138d5d74ccd5f6303c08e15a9896074fe5
Nicolas Wolf <nicolas.wolf@headintheclouddev.com>
2016-06-01 14:25:00 -0700

    N/Email - Added documentation for methods

--- N/email.d.ts
+++ N/email.d.ts
@@ -33,17 +33,27 @@ interface SendCampaignOptions {
 
 interface EmailSendFunction {
     (options: SendOptions): void;
-    promise(options: SendOptions): Promise<void>;
+    promise (options: SendOptions): Promise<void>;
 }
 
 interface EmailSendCampaignFunction {
     (options: SendCampaignOptions): number;
-    promise(options: SendCampaignOptions): Promise<number>;
+    promise (options: SendCampaignOptions): Promise<number>;
 }
 
 interface EmailModule {
+    /**
+     * Method used to send transactional email asynchronously and receive bounceback notifications if the email is not successfully delivered.
+     */
     send: EmailSendFunction;
+    /**
+     * This method is used to send bulk email when a bounceback notification is not required.
+     */
     sendBulk: EmailSendFunction;
+    /**
+     * Method used to send a single “on-demand” campaign email to a specified recipient and return a campaign response ID to track the email.
+     * Email (campaignemail) sublists are not supported. The campaign must use a Lead Nurturing (campaigndrip) sublist.
+     */
     sendCampaign: EmailSendCampaignFunction;
 }
 
