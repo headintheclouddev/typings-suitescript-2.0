60552c713d811b6d3f5251f535eef3c38195208c
John Ogle <john@johnogle.info>
2016-07-20 18:05:41 -0700

    Update fieldChanged and validateField context names

--- N/index.d.ts
+++ N/index.d.ts
@@ -144,8 +144,8 @@ export namespace EntryPoints {
             currentRecord: N_record.ClientCurrentRecord;
             sublistId: string;
             fieldId: string;
-            lineNum: number;
-            columnNum: number;
+            line: number;
+            column: number;
         }
         type fieldChanged = (scriptContext?: fieldChangedContext) => void;
 
@@ -193,8 +193,8 @@ export namespace EntryPoints {
             currentRecord: N_record.ClientCurrentRecord;
             sublistId: string;
             fieldId: string;
-            lineNum?: number;
-            columnNum?: number;
+            line?: number;
+            column?: number;
         }
         type validateField = (scriptContext?: validateFieldContext) => boolean;
 
