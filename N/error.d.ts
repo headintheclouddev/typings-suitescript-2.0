d5e818e45e97ee7b4020e142fae3b8cbfc034579
Robin Mitchell <robin.mitchell@headintheclouddev.com>
2016-07-28 16:58:46 -0700

    N/error: Documentation commenting, add SuiteScriptError.cause

--- N/error.d.ts
+++ N/error.d.ts
@@ -5,8 +5,17 @@ interface EventType {
 }
 
 interface CreateOptions {
+    /**
+     * A user-defined name (error code).
+     */
     name: string;
+    /**
+     * The error message displayed. This value displays on the Execution Log, in the Details column.
+     */
     message: string;
+    /**
+     * Sets whether email notification is suppressed. The default value is false.
+     */
     notifyOff?: boolean;
 }
 
@@ -14,23 +23,55 @@ interface CreateOptions {
  * Encapsulates a SuiteScript error thrown by any script type that is not a user event script.
  */
 export interface SuiteScriptError {
-    toString(): string;
+    /**
+     * Error ID that is automatically generated when a new error is created.
+     */
     id: string;
+    /**
+     * Text that displays on the SuiteScript Execution Log, in the Details column.
+     */
     message: string;
+    /**
+     * A user-defined name (error code).
+     */
     name: string;
+    /**
+     * A list of method calls that the script is executing when the error is thrown. The most recently executed method is listed at the top.
+     */
     stack: string[];
+    /**
+     * The cause of the error message.
+     */
+    cause: Object|Object[]|string|string[]|number|number[]|boolean|boolean[];
 }
     
 /**
  * Encapsulates a SuiteScript error thrown by a user event script.
  */
 export interface UserEventError {
-    toString(): string;
+    /**
+     * The user event type. Holds one of the following values: beforeLoad, beforeSubmit, afterSubmit.
+     */
     eventType: EventType;
+    /**
+     * Error ID that is automatically generated when a new error is created.
+     */
     id: string;
+    /**
+     * Text that displays on the SuiteScript Execution Log, in the Details column.
+     */
     message: string;
+    /**
+     * A user-defined name (error code).
+     */
     name: string;
+    /**
+     * The internal ID of the submitted record that triggered the script. This property only holds a value when the error is thrown by an afterSubmit user event script.
+     */
     recordId: string;
+    /**
+     * A list of method calls that the script is executing when the error is thrown. The most recently executed method is listed at the top.
+     */
     stack: string[];
 }
 
