3a606210581d17126787e9de93071ba0f2be7234
John Ogle <john@johnogle.info>
2016-08-04 13:57:11 -0700

    Util updates

--- N/util.d.ts
+++ N/util.d.ts
@@ -1,15 +1,10 @@
-interface UtilInterface {
-    each: any;
-    extend: any;
-    isArray: (toTest: any) => boolean;
-    isBoolean: (toTest: any) => boolean;
-    isDate: (toTest: any) => boolean;
-    isFunction: (toTest: any) => boolean;
-    isNumber: (toTest: any) => boolean;
-    isObject: (toTest: any) => boolean;
-    isRegExp: (toTest: any) => boolean;
-    isString: (toTest: any) => boolean;
-    trim: (toTrim: string) => string;
-}
-
-export var util: UtilInterface;
+export function isArray(obj: Object): boolean;
+export function isBoolean(obj: Object): boolean;
+export function isDate(obj: Object): boolean;
+export function isNumber(obj: Object): boolean;
+export function isObject(obj: Object): boolean;
+export function isRegExp(obj: Object): boolean;
+export function isString(obj: Object): boolean;
+export function nanoTime(): string;
+export function each(iterable: Object, callback: (object: Object, index?: number) => void): void;
+export function extend(receiver: Object, contributor: Object): Object;
