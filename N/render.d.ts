0fc0bb7032449d0eee0e458c5f7079b0e24b37e8
Robin Mitchell <robin.mitchell@headintheclouddev.com>
2016-08-10 12:15:16 -0700

    N/render: Documentation commenting

--- N/render.d.ts
+++ N/render.d.ts
@@ -4,67 +4,78 @@ import {ServerResponse} from './http';
 import {Result} from './search';
 
 interface AddCustomDataSourceOptions {
-    /**
-     * Data source alias.
-     */
+    /** Data source alias. */
     alias: string;
-    /**
-     * Data format, uses the render.DataSource enum.
-     */
+    /** Data format, uses the render.DataSource enum. */
     format: DataSource;
-    /**
-     * Object, document, or string.
-     */
+    /** Object, document, or string. */
     data: Object | Document | string;
 }
 
 interface AddRecordOptions {
-    /**
-     * Name of the template.
-     */
+    /** Name of the template. */
     templateName: string;
-    /**
-     * The loaded record object to add.
-     */
+    /** The loaded record object to add. */
     record: Record;
 }
 
 interface AddSearchResultsOptions {
+    /** Name of the template. */
     templateName: string;
+    /** The search result to add. */
     searchResult: Result;
 }
 
 interface BOMOptions {
+    /** The internal ID of the bill of material to print. */
     entityId: number;
+    /** The print output type. Set using the render.PrintMode enum. */
     printMode?: PrintMode;
 }
 
 interface EmailMergeResult {
+    /** The body of the email distribution in string format. */
     body: string;
+    /** The subject of the email distribution in string format. */
     subject: string;
 }
 
 interface MergeEmailOptions {
+    /** Internal ID of the template. */
     templateId: number;    // One of the below fields must be included.
-    entity?: RecordRef;
-    recipient?: RecordRef;
-    customRecord?: RecordRef;
+    /** Entity record reference. For example, an employee. */
+    entity?: RecordRef; 
+    /** Recipient record reference.  For example, a lead. */
+    recipient?: RecordRef; 
+    /** Custom record reference. */
+    customRecord?: RecordRef; 
+    /** Support Case ID. */
     supportCaseId?: number;
+    /** Transaction ID. */ 
     transactionId?: number;
 }
 
 interface PackingSlipOptions {
+    /** The internal ID of the packing slip to print. */
     entityId: number;
+    /** The print output type. Set using the render.PrintMode enum. */
     printMode?: PrintMode;
+    /** The packing slip form number. */
     formId?: number;
+    /** Fulfillment ID number. */
     fulfillmentId?: number;
 }
 
 interface PickingTicketOptions {
+    /** The internal ID of the picking ticket to print. */
     entityId: number;
+    /** The print output type. Set using the render.PrintMode enum. */
     printMode?: PrintMode;
+    /** The packing slip form number. */
     formId?: number;
+    /** Shipping group for the ticket. */
     shipgroup?: number;
+    /** Location for the ticket. */
     location?: number;
 }
 
@@ -74,71 +85,90 @@ interface RecordRef {
 }
 
 interface RenderToResponseOptions {
+    /** Response that will be written to PDF. For example, the response passed from a Suitelet. */
     response: ServerResponse;
 }
 
 interface StatementOptions {
+    /** The internal ID of the statement to print. */
     entityId: number;
+    /** The print output type. Set using the render.PrintMode enum. */
     printMode?: PrintMode;
+    /** Internal ID of the form to use to print the statement. */
     formId?: number;
+    /** Date of the oldest transaction to appear on the statement. */
     startDate?: Date;
+    /** Statement date. */
     statementDate?: Date;
-    TransactionsOnly?: boolean;
+    /** Include only open transactions. */
+    openTransactionsOnly?: boolean;
 }
 
 interface XMLToPDFOptions {
+    /** XML document or string to convert to PDF. */
     xmlString: string;
 }
 
 interface TemplateRenderer {
-    /**
-     * Adds XML or JSON as custom data source to an advanced PDF/HTML template.
-     */
+    /** Adds XML or JSON as custom data source to an advanced PDF/HTML template. */
     addCustomDataSource(options: AddCustomDataSourceOptions): void;
-    /**
-     * Binds a record to a template variable.
-     */
+    /** Binds a record to a template variable. */
     addRecord(options: AddRecordOptions): void;
+    /** Binds a search result to a template variable. */
     addSearchResults(options: AddSearchResultsOptions): void;
+    /** Uses the advanced template to produce a PDF printed form. */
     renderAsPdf(): File;
+    /** Renders a server response into a PDF file. */
     renderAsPdfToResponse(options: RenderToResponseOptions): void;
+    /** Return template content in string form. */
     renderAsString(): string;
+    /** Writes template content to a server response. */
     renderToResponse(options: RenderToResponseOptions): void;
-    /**
-     * Sets the template using the internal ID.
-     */
+    /** Sets the template using the internal ID. */
     setTemplateById(options: { id: number; }): void;
-    /**
-     * Sets the template using the script ID.
-     */
+    /** Sets the template using the script ID. */
     setTemplateByScriptId(options: { scriptId: string; }): void;
+    /** Content of template. */
     templateContent: string;
 }
 
 interface TransactionOptions {
+    /** The internal ID of the transaction to print. */
     entityId: number;
+    /** The print output type. Set using the render.PrintMode enum. */
     printMode?: PrintMode;
+    /** The transaction form number. */
     formId?: number;
 }
 
+/** Use this method to create a PDF or HTML object of a bill of material. */
 export function bom(options: BOMOptions): File;
+/** Use this method to produce HTML and PDF printed forms with advanced PDF/HTML templates. */
 export function create(): TemplateRenderer;
+/** Creates a render.EmailMergeResult object for a mail merge with an existing scriptable email template. */
 export function mergeEmail(options: MergeEmailOptions): EmailMergeResult;
+/** Use this method to create a PDF or HTML object of a packing slip. */
 export function packingSlip(options: PackingSlipOptions): File;
+/** Use this method to create a PDF or HTML object of a picking ticket. */
 export function pickingTicket(options: PickingTicketOptions): File;
+/** Use this method to create a PDF or HTML object of a statement. */
 export function statement(options: StatementOptions): File;
+/** Use this method to create a PDF or HTML object of a transaction. */
 export function transaction(options: TransactionOptions): File;
+/** Method used to pass XML to the Big Faceless Organization tag library (which is stored by NetSuite), and return a PDF file. */
 export function xmlToPdf(options: XMLToPDFOptions): File;
 
+/** Holds the string values for supported data source types. Use this enum to set the options.format parameter. */
 export enum DataSource {
     JSON,
     OBJECT,
     XML_DOC,
-    XML_STRING,
+    XML_STRING
 }
 
+/** Holds the string values for supported print output types. Use this enum to set the options.printMode parameter. */
 export enum PrintMode {
     DEFAULT,
     HTML,
-    PDF,
+    PDF
 }
