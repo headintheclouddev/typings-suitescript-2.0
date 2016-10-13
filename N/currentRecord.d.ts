ba482fb046c01cc34166f3687c6b5040f961781a
John Ogle <john@johnogle.info>
2016-10-13 16:59:10 -0700

    N/currentRecord: Remove 2016.1 exports

--- N/currentRecord.d.ts
+++ N/currentRecord.d.ts
@@ -1,16 +1,3 @@
-/* 2016.1: This is an undocumented module.  Use this when you have a UI button in a 
-   client View mode, as these are the only attributes of the current record
-   that are available in View context.
-   If you are working in create/edit mode, use the ClientCurrentRecord object
-   defined in scriptTypes/scriptEntry.d.ts.  It's more similar to to a full
-   Record object. */
-export var id: string;
-export var type: string;
-
-// ^ Remove stuff above here when 2016.2 goes live!
-
-/// <reference path="typings/index.d.ts" />
-
 import {ClientCurrentRecord} from './record';
 
 interface GetCurrentRecordFunction {
