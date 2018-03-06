714e33049fb3006981b122ec10b723d96ea395de
Jeffrey Kabot <jeffrey.m.kabot@gmail.com>
2018-03-06 14:50:41 -0500

    separately test declaration files with strict compiler options
    fix npm test script to work on windows

---
+++ spec/declarationCompileSpec.ts
@@ -0,0 +1,8 @@
+import { exec, rm } from 'shelljs'
+
+describe('declaration files compilation', () => {
+	it('should compile all declaration files without error', () => {
+		let execOut = exec('node node_modules/typescript/bin/tsc -p spec/assets/specs_declarations_tsconfig.json')
+		expect(execOut.code).toBe(0, execOut.stdout)
+	})
+})
