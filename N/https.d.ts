6efeaea3e2c0cc41460fd4c52522de98a968715d
Darren Hill <darren@darrenhillconsulting.ca>
2018-10-28 11:52:33 -0400

    Update https.d.ts
    Re-exported some borrowed http methods

--- N/https.d.ts
+++ N/https.d.ts
@@ -109,7 +109,7 @@ export interface SecureString {
     toString(): string;
 }
 
-export {ClientResponse, ServerRequest, ServerResponse, RequestOptions} from './http';
+export {ClientResponse, ServerRequest, ServerResponse, GetOptions, DeleteOptions, PostOptions, PutOptions, RequestOptions} from './http';
 
 // METHODS \\
 /**
