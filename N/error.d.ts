25b8df6374f63ea10457b26d29a944cc7e57a793
Robin Mitchell <robin.mitchell@headintheclouddev.com>
2019-06-14 07:04:35 +0100

    N/error: Allow the error message to be an Error object instead of just a string.

--- N/error.d.ts
+++ N/error.d.ts
@@ -1,86 +1,49 @@
 interface EventType {
-    beforeLoad: string;
+    beforeLoad:   string;
     beforeSubmit: string;
-    afterSubmit: string;
+    afterSubmit:  string;
 }
 
 interface CreateOptions {
-    /**
-     * A user-defined name (error code).
-     */
+    /** A user-defined name (error code). */
     name: string;
-    /**
-     * The error message displayed. This value displays on the Execution Log, in the Details column.
-     */
-    message: string;
-    /**
-     * Sets whether email notification is suppressed. The default value is false.
-     */
+    /** The error message displayed. This value displays on the Execution Log, in the Details column. */
+    message: string | Error | SuiteScriptError | UserEventError;
+    /** Sets whether email notification is suppressed. The default value is false. */
     notifyOff?: boolean;
 }
 
-/**
- * Encapsulates a SuiteScript error thrown by any script type that is not a user event script.
- */
+/** Encapsulates a SuiteScript error thrown by any script type that is not a user event script. */
 export interface SuiteScriptError {
-    /**
-     * Error ID that is automatically generated when a new error is created.
-     */
+    /** Error ID that is automatically generated when a new error is created. */
     readonly id: string;
-    /**
-     * Text that displays on the SuiteScript Execution Log, in the Details column.
-     */
+    /** Text that displays on the SuiteScript Execution Log, in the Details column. */
     readonly message: string;
-    /**
-     * A user-defined name (error code).
-     */
+    /** A user-defined name (error code). */
     readonly name: string;
-    /**
-     * A list of method calls that the script is executing when the error is thrown. The most recently executed method is listed at the top.
-     */
+    /** A list of method calls that the script is executing when the error is thrown. The most recently executed method is listed at the top. */
     readonly stack: string[];
-    /**
-     * The cause of the error message.
-     */
+    /** The cause of the error message. */
     readonly cause: any;
-    
-    /** 
-     * Whether email notification is suppressed. 
-     */
+    /** Whether email notification is suppressed. */
     notifyOff?: boolean;
 }
     
-/**
- * Encapsulates a SuiteScript error thrown by a user event script.
- */
+/** Encapsulates a SuiteScript error thrown by a user event script. */
 export interface UserEventError {
-    /**
-     * The user event type. Holds one of the following values: beforeLoad, beforeSubmit, afterSubmit.
-     */
+    /** The user event type. Holds one of the following values: beforeLoad, beforeSubmit, afterSubmit. */
     readonly eventType: EventType;
-    /**
-     * Error ID that is automatically generated when a new error is created.
-     */
+    /**  Error ID that is automatically generated when a new error is created. */
     readonly id: string;
-    /**
-     * Text that displays on the SuiteScript Execution Log, in the Details column.
-     */
+    /** Text that displays on the SuiteScript Execution Log, in the Details column. */
     readonly message: string;
-    /**
-     * A user-defined name (error code).
-     */
+    /** A user-defined name (error code). */
     readonly name: string;
-    /**
-     * The internal ID of the submitted record that triggered the script. This property only holds a value when the error is thrown by an afterSubmit user event script.
-     */
+    /** The internal ID of the submitted record that triggered the script. This property only holds a value when the error is thrown by an afterSubmit user event script. */
     readonly recordId: string;
-    /**
-     * A list of method calls that the script is executing when the error is thrown. The most recently executed method is listed at the top.
-     */
+    /** A list of method calls that the script is executing when the error is thrown. The most recently executed method is listed at the top. */
     readonly stack: string[];
 }
 
-/**
- * Creates a new error.SuiteScriptError or error.UserEventError object.
- */
+/** Creates a new error.SuiteScriptError or error.UserEventError object. */
 export function create<T extends SuiteScriptError | UserEventError = SuiteScriptError>(options: CreateOptions): T;
