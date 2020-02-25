4e36441587fe04568d7247bbcdac53bcf3b61e5e
Robin Mitchell <robin.mitchell@headintheclouddev.com>
2020-02-25 10:30:45 +0000

    Suitelet Sample: Update formatting

--- spec/assets/Suitelet1Sample.ts
+++ spec/assets/Suitelet1Sample.ts
@@ -3,10 +3,11 @@
  * @NScriptType Suitelet
  */
 
-import { EntryPoints } from 'N/types'
-import { load } from 'N/record'
-export var onRequest: EntryPoints.Suitelet.onRequest = (ctx) => {
-    var folder = load({ type: 'folder', id: 36464 })
-    var allfields = folder.getFields().join(', ')
-    ctx.response.write(`<br>all fields: ${allfields}`) 
-}
+import {EntryPoints} from 'N/types';
+import record = require('N/record');
+
+export const onRequest: EntryPoints.Suitelet.onRequest = (ctx) => {
+  const folder    = record.load({ type: 'folder', id: 36464 });
+  const allFields = folder.getFields().join(', ');
+  ctx.response.write(`<br>All fields: ${allFields}`);
+};
