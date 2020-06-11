885405e2a7cea44d764155ce4c055ffa4589cf09
Robin Mitchell <robin.mitchell@headintheclouddev.com>
2020-06-11 11:16:42 +0100

    Client script example formatting updates

--- example-ClientScript.ts
+++ example-ClientScript.ts
@@ -4,33 +4,21 @@
  */
 
 import {EntryPoints} from 'N/types'
-import * as search from 'N/search'
+import search = require('N/search');
 
 export function pageInit(ctx: EntryPoints.Client.pageInitContext) {
-
     if (ctx.mode != 'edit') return;
 
     search.create.promise({
         type: 'customer',
-        filters: [
-            search.createFilter({
-                name: 'companyname',
-                operator: search.Operator.ISNOTEMPTY
-            }),
-        ],
+        filters: [search.createFilter({ name: 'companyname', operator: search.Operator.ISNOTEMPTY })],
         columns: [
-            search.createColumn({
-                name: 'companyname',
-                sort: search.Sort.ASC
-            }),
+            search.createColumn({ name: 'companyname', sort: search.Sort.ASC }),
             { name: 'email', },
             'fax',
         ],
     }).then(search => {
-        return search.run().getRange.promise({
-            start: 0,
-            end: 1
-        });
+        return search.run().getRange.promise({ start: 0, end: 1 });
     }).then(results => {
         if (results.length === 0) return alert("No companies");
         alert(`First company alphabetically: ${results[0].getValue('companyname')}`);
