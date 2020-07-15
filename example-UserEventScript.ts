2fe7c6f9f11926dd77cb10a48c8bb83aeee21707
Robin Mitchell <robin.mitchell@headintheclouddev.com>
2020-07-15 11:39:55 +0100

    User event example: formatting updates

--- example-UserEventScript.ts
+++ example-UserEventScript.ts
@@ -4,12 +4,10 @@
  */
 
 import {EntryPoints} from 'N/types'
-import * as log from 'N/log'
+import log = require('N/log');
 
 export function beforeSubmit(ctx: EntryPoints.UserEvent.beforeSubmitContext) {
-
-    let x = ctx.newRecord.getValue({fieldId: 'companyname'})
-
-    log.audit('value', `companyname is: ${x}`)
-
+  if (ctx.type == ctx.UserEventType.CREATE) {
+    log.debug('Entity Id', ctx.newRecord.getValue('entity')); // When creating a transaction from an entity, log the entity internal id
+  }
 }
