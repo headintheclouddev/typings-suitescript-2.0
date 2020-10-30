f1ecb0af2813bbde9e62c03018bf4607727901b9
Robin Mitchell <robin.mitchell@headintheclouddev.com>
2020-10-30 08:33:54 +0000

    Client script example: Add a select field to the lookup, change date to string

--- example-ClientScript.ts
+++ example-ClientScript.ts
@@ -10,10 +10,11 @@ export function pageInit(ctx: EntryPoints.Client.pageInitContext) {
   if (ctx.mode != 'edit') return;
 
   const customerId = ctx.currentRecord.getValue('entity'); // Assume this script is running on a transaction
-  search.lookupFields.promise({ type: 'customer', id: customerId, columns: ['companyname', 'datecreated'] }).then((values) => {
-    const name = values.companyname as string;
-    const date = values.datecreated as Date;
-    console.log('Customer', name, 'created at', date);
+  search.lookupFields.promise({ type: 'customer', id: customerId, columns: ['companyname', 'datecreated', 'entitystatus'] }).then((values) => {
+    const name   = values.companyname as string;
+    const date   = values.datecreated as string;
+    const status = values.entitystatus as { value: string, text: string }[];
+    console.log('Customer', name, 'created at', date, 'status', status);
   });
 
   search.create.promise({
