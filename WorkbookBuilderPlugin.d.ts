4c0e2a5bb90042a22b4367f4b6d0fa902d8acf69
Robin Mitchell <robin.mitchell@headintheclouddev.com>
2021-11-18 09:19:42 +0000

    Remove DataSetBuilderPlugin and WorkbookBuilderPlugin definitions, as they are now included in EntryPoints.Plugin

--- WorkbookBuilderPlugin.d.ts
+++
@@ -1,9 +0,0 @@
-import {Workbook} from "N/workbook";
-
-interface CreateWorkbookContext {
-  readonly description: string;
-  readonly name: string;
-  readonly owner: number;
-  readonly role: number;
-  workbook: Workbook;
-}
