7903876a3711ab3796a390286b985efa0ea760d5
Alex Howley <howley.alex@gmail.com>
2024-07-26 20:41:58 -0500

    clean up field schema example and add setValue example

--- example-Record-generics.ts
+++ example-Record-generics.ts
@@ -4,22 +4,25 @@
  */
 
 import { EntryPoints } from "N/types";
-import { load as loadRecord, Type as RecordType, RecordFieldsMap } from "N/record";
+import { load as loadRecord, Type as RecordType, RecordFieldSchema } from "N/record";
 
 type InternalId = `${number}`;
 
-interface BillFieldsMap extends RecordFieldsMap {
+interface BillFieldSchema extends RecordFieldSchema {
     tranid: string;
-    subsidiary: InternalId;
     trandate: Date;
+    subsidiary: InternalId;
 }
 
 /**
  * By passing a generic to "load record" that maps each field ID to its JavaScript return type,
  * types are now inferred automatically when using "getValue".
+ *
+ * Types are also inferred for "setValue", helping ensure you are using the type any given
+ * field id expects.
  */
 function withFieldsMapGeneric() {
-    const bill = loadRecord<BillFieldsMap>({
+    const bill = loadRecord<BillFieldSchema>({
         type: RecordType.VENDOR_BILL,
         id: 1,
     });
@@ -31,6 +34,10 @@ function withFieldsMapGeneric() {
     billTranId.split("0");
     billDate.getUTCMilliseconds();
     billUnmapped.toString();
+
+    /* This line will cause compile-time error, instead of one at runtime: */
+    // bill.setValue("subsidiary", "NaN");  # Argument of type '"NaN"' is not assignable to parameter of type '`${number}`'.
+    bill.setValue("subsidiary", "3");
 }
 
 /**
@@ -49,9 +56,12 @@ function withoutFieldsMapGeneric() {
     // /* This line would now cause a compile-time error: */
     // billDate.getUTCMilliseconds();  # Property 'getUTCMilliseconds' does not exist on type 'FieldValue'. Property 'getUTCMilliseconds' does not exist on type 'string'.
 
-    (billTranId as InternalId).split("0");
+    (billTranId as string).split("0");
     (billDate as Date).getUTCMilliseconds();
     billUnmapped.toString();
+
+    /* This line does not cause a compile-time error, meaning this bug will only surface at runtime: */
+    bill.setValue("subsidiary", "NaN");
 }
 
 export const pageInit: EntryPoints.Client.pageInit = (_context) => {
