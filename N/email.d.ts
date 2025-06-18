85356eab44ab5a6419616085f12ad6d36a0ff530
Simeon Bartley <simeon.bartley@unicarehealth.com.au>
2025-06-18 08:42:33 +0800

    Add readonly array parameters  to email functions

--- N/email.d.ts
+++ N/email.d.ts
@@ -11,7 +11,7 @@ interface SendOptions {
      * A maximum of 10 recipients (recipient + cc + bcc) is allowed.
      * Note: Only the first recipient displays on the Communication tab (under the Recipient column).
      */
-    recipients: number[]|string[]|number|string;
+    recipients: number|string|number[]|string[]|readonly number[]|readonly string[];
     /**
      * The email address that appears in the reply-to header when an email is sent out.
      */
@@ -21,13 +21,13 @@ interface SendOptions {
      * For multiple recipients, use an array of internal IDs or email addresses. You can use an array that contains a combination of internal IDs and email addresses.
      * A maximum of 10 recipients (recipient + cc + bcc) is allowed.
      */
-    cc?: string[]|number[];
+    cc?: number[]|string[]|readonly number[]|readonly string[];
     /**
      * The internal ID or email address of the secondary recipient to blind copy.
      * For multiple recipients, use an array of internal IDs or email addresses. You can use an array that contains a combination of internal IDs and email addresses.
      * A maximum of 10 recipients (recipient + cc + bcc) is allowed.
      */
-    bcc?: string[]|number[];
+    bcc?: number[]|string[]|readonly number[]|readonly string[];
     /**
      * Subject of the outgoing message
      */
@@ -41,7 +41,7 @@ interface SendOptions {
      * An individual attachment must not exceed 5MB and the total message size must be 15MB or less.
      * Note: Supported for server-side scripts only.
      */
-    attachments?: File[];
+    attachments?: File[]|readonly File[];
     /**
      * Object that contains key/value pairs to associate the Message record with related records (including custom records).
      */
@@ -49,7 +49,7 @@ interface SendOptions {
     /**
      * If true, the Message record is not visible to an external Entity (for example, a customer or contact). Default is false.
      */
-    isInternalOnly?: boolean;   
+    isInternalOnly?: boolean;
 }
 
 interface RelatedRecordTypes {
